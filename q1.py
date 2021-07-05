e9f4bb9cd42959c6608de8d4554c834e1660a4c7
Arun652 <70481319+Arun652@users.noreply.github.com>
2021-07-06 01:19:43 +0530

    Assigment1 q1

---
+++ q1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+a=np.arange(40,50)
+b=np.arange(50,60)
+plt.plot(a,b)
+plt.title("Q1")
+plt.xlabel("a")
+plt.ylabel("b")
+
+
+# In[ ]:
+
+
+
+
